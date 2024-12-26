13729f2f8a494d670057b6479592f62ebf78812e
phuchoclaptrinh <168012773+phuchoclaptrinh@users.noreply.github.com>
2024-12-26 08:04:08 +0700

    Delete backend directory

--- backend/home/permissions.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-class IsEmployee(BasePermission):
-    """
-    Chỉ cho phép nhân viên hoặc admin truy cập.
-    """
-    def has_permission(self, request, view):
-        return request.user.is_staff  # Chỉ cho phép người dùng có quyền admin/staff
-
--- backend/users/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-from django.contrib.auth.forms import AuthenticationForm
-
-class LoginForm(AuthenticationForm):
-    user_form = forms.CharField(label='Email hoặc số điện thoại')
